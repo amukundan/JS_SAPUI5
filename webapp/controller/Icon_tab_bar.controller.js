24fb0f8b0e191d0ae6b283a4326efeb9adf0b5f1
amukundan <ajaymukundan@gmail.com>
2020-07-24 13:15:55 +0000

    icon tab bar week1 assignment

--- webapp/controller/Icon_tab_bar.controller.js
+++ webapp/controller/Icon_tab_bar.controller.js
@@ -5,7 +5,26 @@ sap.ui.define([
 
 	return Controller.extend("Week1_Assignment.ZG61_ICON_TAB_BAR_WEEK1_ASSG.controller.Icon_tab_bar", {
 		onInit: function () {
+			/*	if (!this.oPersonalInfo) {
+					this.oPersonalInfo = sap.ui.xmlfragment("Week1_Assignment.ZG61_ICON_TAB_BAR_WEEK1_ASSG.fragments.personalinformation", this)
+				},*/
 
-		}
+		},
+		handleIconTabBarSelect: function () {
+			//set the values in fragments from corresponding xml view controls
+			this.byId("inpfrgsummryFirst").setValue(this.byId("inpfrgFirst").getValue());
+			this.byId("inpfrgsummryLast").setValue(this.byId("inpfrgLast").getValue());
+			this.byId("iddpfrgsummaryDoB").setValue(this.byId("iddpfrgDoB").getValue());
+			this.byId("inpfrgsummryline1").setValue(this.byId("inpfrgline1").getValue());
+			this.byId("inpfrgsummryline2").setValue(this.byId("inpfrgline2").getValue());
+
+			this.byId("inpfrgsummryHD").setValue(this.byId("inpfrgHD").getValue());
+			this.byId("inpfrgsummryUniv").setValue(this.byId("inpfrgUniv").getValue());
+			this.byId("iddpfrgsummaryMop").setValue(this.byId("iddpfrgMop").getValue());
+			return;
+		},
+		/*handleNext: function () {
+
+		},*/
 	});
 });
--- webapp/test/unit/allTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"Week1_Assignment/ZG61_ICON_TAB_BAR_WEEK1_ASSG/test/unit/controller/Icon_tab_bar.controller"
-], function () {
-	"use strict";
-});
